84ed139dc771d8b963ed7a350b88469bbc2b346f
Nathan Isaac <nathan@nathan-isaac.com>
2015-07-03 17:42:06 -0700

    Add blade forelse to index for cleaner code

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -4,17 +4,15 @@
 
     <h1>All Articles</h1>
 
-    @foreach($articles as $article)
+    @forelse($articles as $article)
         @include('articles.partials.article')
-    @endforeach
-
-    @unless( ! $articles->isEmpty())
+    @empty
         <div class="panel panel-default">
             <div class="panel-body">
                 No articles found.
             </div>
         </div>
-    @endunless
+    @endforelse
 
     {!! $articles->render() !!}
 @endsection
